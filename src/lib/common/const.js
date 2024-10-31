33ee88434458af720afac4fb3fa12a1be8ae940b
Aco Šmrkas <ergexplorer@gmail.com>
2024-10-31 12:45:25 +0100

    test address

--- src/lib/common/const.js
+++ src/lib/common/const.js
@@ -12,6 +12,6 @@ export const DEV_PK    = '9f9ApDfCA7BcPEoSQKgFzsTdBmdV31hLxoWZt4wJgcwr7mtQ6qp';
 export const BLOCKFROST_PROJECT_ID = 'mainnetKDqLJpkxQiZak5FPBG0BN2KHJHr6HYhC';
 
 export const RECIPIENT_ADDRESS_CARDANO = 'addr1q82946608zxkl8v2juvgptrkp64hktzs57m8rkffd42mpd3x27743ccqz5v5uvsqttwhm9el0wyqrl9nt3gchg69scgss5lul0';
-export const RECIPIENT_ADDRESS_ERGO = "9hq7R1zrrUAmLp2Row7tMJwXhFYxSQf2Zn8UDrN6zJhzNQnnPiF";
+export const RECIPIENT_ADDRESS_ERGO = "9fLXRjthKecc7LsHyQAF9w2DfqVbsFxsHqUBpz2ouBPYRmaBxfT";
 export const RSADA_TOKEN_ID = "e023c5f382b6e96fbd878f6811aac73345489032157ad5affb84aefd4956c297";
 export const RSERG_POLICY_ID = "04b95368393c821f180deee8229fbd941baaf9bd748ebcdbf7adbb14";
