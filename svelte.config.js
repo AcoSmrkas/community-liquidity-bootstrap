5f20bd5759a40b4b38e7bdbd3619236a916b5f06
hq3r <crooksfinance@gmail.com>
2024-11-26 22:11:43 +0100

    back to default

--- svelte.config.js
+++ svelte.config.js
@@ -3,30 +3,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
-
-    // Set the base path for your production deployment
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/mew-community-liquidity-bootstrap' : ''
-    },
-
-    // Configure the routes
-    routes: (r) => [
-      // Add the /new route
-      { src: '/new', dest: '/new' },
-
-      // Add any other routes here
-      // ...
-    ]
-  }
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		paths: {
+            base: process.env.NODE_ENV === 'production' ? '/mew-community-liquidity-bootstrap' : '',
+        }
+	}
 };
 
 export default config;
